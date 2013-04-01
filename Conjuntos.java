0ea1f2089e9cbfb31e2fb63fce55cdf7f47c09f1
nemesiscodex <nemesiscodex@gmail.com>
2013-03-31 23:35:18 -0300

    commit 5

--- Conjuntos.java
+++
@@ -1,7 +0,0 @@
-public class Conjuntos extends ConjuntoBase{
-	private Double[] pertenencia;
-	public Conjuntos(String[] elementos, Double[]pertenencia){
-		super(elementos);
-		this.pertenencia = pertenencia;
-	}
-}
